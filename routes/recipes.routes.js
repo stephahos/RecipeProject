adce8e878249397e9f04eaa59ad5eb89bd2fb3fa
Stephanie AHossi <ahossi.stephanie@gmail.com>
2022-11-08 11:10:44 +0100

    all recipes page

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -3,14 +3,33 @@ const router = require("express").Router();
 const uploader = require('../config/cloudinary.config');
 
 //GET my profile page
-router.get("/myprofilepage", (req, res, next) => {
+router.get("/myprofilepage", async(req, res, next) => {
   console.log(req.session)
+  //const Recipes = await Recipe.find()
   if (req.session.user) {
     res.render("recipes/myprofilepage", {user:req.session.user})
   } else {
     res.redirect('/auth/login')
   }
 }) 
+
+/* GET All Recipes page */
+router.get('/allRecipes', async (req, res, next) => {
+  try {
+    const Recipes = await Recipe.find()
+    res.render('recipes/allRecipes', { Recipes })
+
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+//Get Recipe by ID => Recipe Details
+router.get('/allRecipes/:recipeId', async (req, res, next) => {
+  console.log(req.params.recipeId)
+  const recipe = await Recipe.findById(req.params.recipeId)
+  res.render('recipes/recipe-details', { recipe })
+})
 
 /* GET search page */
 router.get("/search", (req, res, next) => {
@@ -28,7 +47,7 @@ router.post('/create', uploader.single("imageUrl"),  async (req, res) => {
     try {
       await Recipe.create({
         name: req.body.name,
-        ingredients: req.body.ingredients.split(''),
+        ingredients: req.body.ingredients,
         instructions: req.body.instructions,
         cooktime: req.body.cooktime,
         level: req.body.level,
@@ -46,5 +65,23 @@ router.post('/create', uploader.single("imageUrl"),  async (req, res) => {
         console.log('render the recipe creation form view so the user can try again')
     }
   })
+
+//to Update a recipe
+router.get('/update/:recipeId', async (req, res) => {
+  const recipe = await Recipe.findById(req.params.recipeId)
+  res.render('updated-movie', { recipe })
+})
+
+router.post('/update/:recipeId', async (req, res) => {
+  console.log(req.body)
+  await Recipe.findByIdAndUpdate(req.params.recipeId, { ...req.body, name: req.body.name })
+  res.redirect(`/recipes/${req.params.recipeId}`)
+})
+
+
+
+
+
+
 
 module.exports = router;
